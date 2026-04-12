721dd3ec85f10df10e5a09722e036e4d463408ef
Rodel Boc <r.boc@applyminds.com>
2026-04-12 12:56:02 +0800

    App Update - 20260412-3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone", // 👈 ADD THIS
   experimental: {
-    serverComponentsExternalPackages: ["@sparticuz/chromium-min"],
+    serverComponentsExternalPackages: [
+      "@sparticuz/chromium-min",
+      "puppeteer-core",
+    ],
   },
 };
 
